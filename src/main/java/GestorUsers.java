402e5f12043342ed9ad3df0e9b6439960537a5a9
raaltz <ruben.lopesmp@gmail.com>
2025-03-31 12:30:28 +0100

    Gestor Users

--- src/main/java/GestorUsers.java
+++ src/main/java/GestorUsers.java
@@ -1,7 +1,26 @@
 import java.util.LinkedList;
 
 public class GestorUsers {
-    LinkedList<User> listaUsers = new LinkedList<>();
+    private LinkedList<User> listaUsers;
 
+    public GestorUsers(LinkedList<User> listaUsers) {
+        this.listaUsers = new LinkedList<>();
+    }
 
+    public void addUser(String name, String gender, int dayOfBirth, int monthOfBirth, int yearOfBirth) {
+        User userAdd = new User(name, gender, dayOfBirth, monthOfBirth, yearOfBirth);
+        listaUsers.add(userAdd);
+    }
+
+    public boolean removerUser(User user){
+        return listaUsers.remove(user);
+    }
+
+    public LinkedList<User> getListaUsers() {
+        return new LinkedList<>(listaUsers);
+    }
+
+    public User getUser(User user) {
+        return listaUsers.get(listaUsers.indexOf(user));
+    }
 }
